9b9b9918c49124a29ecba9bc21fc616443816f34
Aromal004 <aromalaj9111@gmail.com>
2024-07-19 12:06:40 +0530

    for Deploying

--- Registration_form/settings.py
+++ Registration_form/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +81,19 @@ WSGI_APPLICATION = "Registration_form.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Internshipp_Dcube",
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
-    }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "Internshipp_Dcube",
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost'
+#     }
+# }
+
+
+DATABASES ={
+    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- Registration_form/urls.py
+++ Registration_form/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from form import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('form.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
